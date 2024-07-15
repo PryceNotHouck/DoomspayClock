0ed094450a1186a6e28107a09cc4de0067d16427
Pryce Houck <phouck@ufl.edu>
2024-07-14 21:24:44 -0400

    Compiles housing data.

--- Get Data/housing.py
+++ Get Data/housing.py
@@ -1,16 +1,53 @@
 import fetch
-import pandas as pd
+import csv
 import os
+import pandas as pd
 
 
 def housing():
     fetch.get_housing()
     data = []
+    year = 0
+    median = 0
+    change = 0
     file = os.path.join(os.path.dirname(__file__), "Local", "housingprices.csv")
-    df = pd.read_csv(file, skiprows=6)
+    with open(file, newline='') as f:
+        reader = csv.reader(f)
+        for i in range(0, 5):
+            next(reader)
+        for row in reader:
+            if row[0] != 'US':
+                break
+            if row[1][2] == '1':
+                if row[1][-1] == '1':
+                    year = row[1][:4]
+                    median += float(row[3])
+                    change -= float(row[3])
+                if row[1][-1] == '2':
+                    median += float(row[3])
+                    median /= 2
+                    change += float(row[3])
+                    data.append([year, median, change])
+            else:
+                if row[1][-1] == '1':
+                    year = row[1][:4]
+                    median += float(row[3])
+                    change -= float(row[3])
+                if row[1][-1] == '2' or row[1][-1] == '3':
+                    median += float(row[3])
+                if row[1][-1] == '4':
+                    median += float(row[3])
+                    median /= 4
+                    change += float(row[3])
+                    data.append([year, median, change])
+                    year = 0
+                    median = 0
+                    change = 0
+    fetch.flush_local()
 
-    # Idea for this one: Rows are 2000 - 2010, first column is year, second column is median price, third column is
-    # change from Q1 of that year to Q4 (or last available quarter).
+    compiled = pd.DataFrame(data, columns=['Year', 'Average Median Price', 'Year-In Change'])
+    compiled.to_csv('housing.csv', index=False)
+    print("Housing Complete")
 
 
 if __name__ == "__main__":

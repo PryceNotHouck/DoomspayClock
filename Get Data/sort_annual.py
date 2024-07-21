5f54c5ce3c5fcbb1b01847432d0e5b227c456393
Pryce Houck <phouck@ufl.edu>
2024-07-21 16:39:14 -0400

    Writes data for every year into a compiled file of denormalized data.

--- Get Data/sort_annual.py
+++ Get Data/sort_annual.py
@@ -1,25 +1,21 @@
 import csv
 import os
-import pandas as pd
 
 
 def sort_annual():
-    for year in range(1929, 2023):
+    for year in range(1929, 2024):
         data = []
-        columns = []
+        data_columns = []
         annual_data_path = os.path.join(os.path.dirname(__file__), "Annual Data")
         for file in os.listdir(annual_data_path):
             file_path = os.path.join(os.path.dirname(__file__), "Annual Data", file)
-            print(file_path)
             with open(file_path,  newline='') as f:
                 reader = csv.reader(f)
                 set_columns = False
                 for row in reader:
                     if not set_columns:
-                        row.reverse()
-                        row.pop()
-                        row.reverse()
-                        columns += row
+                        row.remove(row[0])
+                        data_columns += row
                         set_columns = True
                         check_next = reader.__next__()
                         if int(check_next[0]) > year:
@@ -27,23 +23,20 @@ def sort_annual():
                                 data.append(0.0)
                             break
                         else:
-                            check_next.reverse()
-                            check_next.pop()
-                            check_next.reverse()
-                            for i in range(0, len(check_next)):
-                                try:
-                                    converted = float(check_next[i])
-                                    check_next[i] = converted
-                                except ValueError:
-                                    check_next[i] = 0.0
-                            data += check_next
+                            if int(check_next[0]) == year:
+                                check_next.remove(check_next[0])
+                                for i in range(0, len(check_next)):
+                                    try:
+                                        converted = float(check_next[i])
+                                        check_next[i] = converted
+                                    except ValueError:
+                                        check_next[i] = 0.0
+                                data += check_next
                         continue
                     if int(row[0]) != year:
                         continue
                     else:
-                        row.reverse()
-                        row.pop()
-                        row.reverse()
+                        row.remove(row[0])
                         for i in range(0, len(row)):
                             try:
                                 converted = float(row[i])
@@ -51,6 +44,13 @@ def sort_annual():
                             except ValueError:
                                 row[i] = 0.0
                         data += row
+
+        write_path = "Years/" + str(year) + "_denormalized.csv"
+        with open(write_path, 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(data_columns)
+            writer.writerow(data)
+        print(year, "- Denormalized, Complete")
 
 
 if __name__ == '__main__':
